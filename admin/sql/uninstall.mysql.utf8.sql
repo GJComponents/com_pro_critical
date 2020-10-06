2585bdc086c91f542958ca630169aabc5023edac
oleg <sad.net79@gmail.com>
2020-10-07 00:06:44 +0300

    version > 1.4.76

--- admin/sql/uninstall.mysql.utf8.sql
+++ admin/sql/uninstall.mysql.utf8.sql
@@ -6,10 +6,13 @@ DROP TABLE IF EXISTS `#__pro_critical_user_agent_browser`;
 DROP TABLE IF EXISTS `#__pro_critical_device_client`;
 DROP TABLE IF EXISTS `#__pro_critical_admins`;
 DROP TABLE IF EXISTS `#__pro_critical_html_task`;
+DROP TABLE IF EXISTS `#__pro_critical_html_setting_type_device_map`;
 DROP TABLE IF EXISTS `#__pro_critical_type_device`;
 DROP TABLE IF EXISTS `#__pro_critical_type_browser`;
 DROP TABLE IF EXISTS `#__pro_critical_directory_components`;
 DROP TABLE IF EXISTS `#__pro_critical_directory_views`;
 DROP TABLE IF EXISTS `#__pro_critical_css_file`;
 DROP TABLE IF EXISTS `#__pro_critical_css_style`;
-DROP TABLE IF EXISTS `#__pro_critical_html_setting_type_device_map`;
+DROP TABLE IF EXISTS `#__pro_critical_js_file`;
+DROP TABLE IF EXISTS `#__pro_critical_js_style`;
+
